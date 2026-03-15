0fcb27aee468e05728e5e22444fdb75613fc1729
Charlatan <134639083+CharlatanAlive@users.noreply.github.com>
2026-03-15 15:12:57 +0600

    Delete Python/Chapter 4/if directory

--- Python/Chapter 4/if/if_2.py
+++
@@ -1,5 +0,0 @@
-name = str(input("enter your name"))
-if name == "":
-    print("you did'nt enter your name")
-else: 
-    print(f"hello {name}")
--- Python/Chapter 4/if/if_4.py
+++
@@ -1,5 +0,0 @@
-online = True
-if online == True:
-    print("you are online")
-else: 
-    print("you are ofline")
