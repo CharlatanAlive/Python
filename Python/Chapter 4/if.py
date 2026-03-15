13fa3ba999e244e55148d1b461b4b54960e0ce6b
Charlatan <134639083+CharlatanAlive@users.noreply.github.com>
2026-03-15 15:13:42 +0600

    Add files via upload

---
+++ Python/Chapter 4/if.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age"))
+if age >= 100:
+    print("you are expierd")
+    
+elif age >= 18:
+    print("you are eligible")
+    
+elif age < 0:
+    print("you haven't been born yet")
+
+else:
+    print("you are not eligible")
+    
+    
---
+++ Python/Chapter 4/if_4.py
@@ -0,0 +1,5 @@
+online = True
+if online == True:
+    print("you are online")
+else: 
+    print("you are ofline")
