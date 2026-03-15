99ef660b5f6136f188bf47fdb7886d0e245ef7a7
Charlatan <134639083+CharlatanAlive@users.noreply.github.com>
2026-03-15 15:12:36 +0600

    Add files via upload

---
+++ Python/Chapter 4/if/if_2.py
@@ -0,0 +1,5 @@
+name = str(input("enter your name"))
+if name == "":
+    print("you did'nt enter your name")
+else: 
+    print(f"hello {name}")
---
+++ Python/Chapter 4/if/if_3.py
@@ -0,0 +1,8 @@
+response = input("would you like to go somewhere? {Y/N} ")
+
+if response == "Y":
+    print("Yay let's go")
+elif response == "N":
+    print ("ahh ok")
+else:
+    print("that's a pity")
